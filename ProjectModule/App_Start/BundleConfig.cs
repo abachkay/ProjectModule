4d6c2c0c2e69b3ba48e76776882023b78af1d60c
Bachkay Oleksandr <abachkay@gmail.com>
2016-12-17 20:36:57 +0200

    change library to bootstrap

--- ProjectModule/App_Start/BundleConfig.cs
+++ ProjectModule/App_Start/BundleConfig.cs
@@ -20,15 +20,16 @@ namespace ProjectModule
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      //"~/Scripts/bootstrap.js",
-                      //"~/Scripts/respond.js"
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"
                       ));
 
             bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
-                      "~/Scripts/materialize.js"));
+                      //"~/Scripts/materialize.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                    //  "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/materialize.css",
                       "~/Content/site.css"));
         }
