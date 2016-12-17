4a51936c8266314d130bc12637b86b388e9be7e3
DESKTOP-2CQSF2A\bachkay <abachkay@gmail.com>
2016-12-17 18:05:31 +0200

    Add project files

---
+++ ProjectModule/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectModule.Models;
+
+namespace ProjectModule.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var db = new ProjectModuleDBEntities())
+            {
+                var tasks = db.Tasks.ToList();
+                ViewBag.Tasks = tasks;
+            }
+            return View();
+        }        
+    }
+}
---
+++ ProjectModule/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectModule.Startup))]
+namespace ProjectModule
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
