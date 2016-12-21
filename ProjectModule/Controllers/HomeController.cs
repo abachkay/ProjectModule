48f621e1a88ff92147eb132663d99c2821ca010b
Bachkay Oleksandr <abachkay@gmail.com>
2016-12-21 15:08:34 +0200

    implemented page select at home

--- ProjectModule/Controllers/HomeController.cs
+++ ProjectModule/Controllers/HomeController.cs
@@ -9,16 +9,29 @@ namespace ProjectModule.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    using (var db = new ProjectModuleDBEntities())
+        //    {
+        //        //var html = "<a href='google.com'></a>";
+        //        //var css = "";
+        //        //ViewBag.V1 = new TaskVerifier(db.Task.FirstOrDefault(), html, css).Verify().ToString();
+        //        //ViewBag.V2 = new TaskVerifier(db.Task.ToList()[1], html, css).Verify().ToString();
+        //        ViewBag.Tasks = db.Task.ToList();
+        //        ViewBag.CurrentTask = db.Task.FirstOrDefault();
+        //    }
+        //    return View();
+        //}
+        [HttpGet]
+        public ActionResult Index(int taskId=0)
         {
             using (var db = new ProjectModuleDBEntities())
-            {
-                var html = "<a href='google.com'></a>";
-                var css = "";
-                ViewBag.V1 = new TaskVerifier(db.Task.FirstOrDefault(), html, css).Verify().ToString();
-                ViewBag.V2 = new TaskVerifier(db.Task.ToList()[1], html, css).Verify().ToString();
+            {                
+                ViewBag.Tasks = db.Task.ToList();
+                var task=db.Task.ToList().Where(x=>x.Id==taskId).FirstOrDefault();
+                ViewBag.CurrentTask = (task == null ?db.Task.FirstOrDefault():task);
             }
             return View();
-        }        
+        }
     }
 }
